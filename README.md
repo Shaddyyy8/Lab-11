4db016e0eb27503c19075c75bc9787bbf2fd4790
Gasinets Dmitry <sin_ner008@mail.ru>
2025-12-16 17:22:24 +0200

    Закинул проект

---
+++ README.md
@@ -0,0 +1,7 @@
+"Mesto"
+
+Ссылка на GitHub
+https://github.com/malchuganchik/mesto-russia.git
+
+Ссылка на Pages 
+https://malchuganchik.github.io/mesto-russia/
---
+++ scripts/initial-cards.js
@@ -0,0 +1,27 @@
+const containerCards = [
+  {
+    name: 'Карачаевск',
+    link: 'https://awm-trade.ru/wp-content/uploads/2016/11/IMG_5512-copy-600x400.jpg'
+    
+  },
+  {
+    name: 'Opel zafira A',
+    link: 'https://s.auto.drom.ru/i24198/c/photos/fullsize/opel/zafira/opel_zafira_542204.jpg'
+  },
+  {
+    name: 'Opel zafira B',
+    link: 'https://img.autogidas.lt/10_1_7192416/opel-zafira-2008-2010.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRmq9-IpbAnv8deqH_IZITHxh1u7Waeqw3un1C8ZDe_9D0bR2vD'
+  },
+  {
+    name: 'Opel zafira C',
+    link: 'https://avatars.mds.yandex.net/get-verba/1030388/2a00000160957a1d9782d074fda36ed4918c/cattouchretcr'
+  },
+  {
+    name: 'Эльбрус',
+    link: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQS3MioiNFTn0Phx0Kuv2RvDI-m102QHwfqmjOAmBQxjHbRLxEN'
+  }
+];
